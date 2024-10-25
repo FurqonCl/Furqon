68e08b1e3ce2cf2586b681e7ec3683d21197f03d
FurqonCl <maufurkonfurkon@gmail.com>
2024-10-25 10:35:13 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("answerYes").addEventListener("click", function() {
+    document.getElementById("response").innerText = "Yay! Aku senang kamu mau!";
+});
+
+document.getElementById("answerNo").addEventListener("click", function() {
+    document.getElementById("response").innerText = "Oh, tidak apa-apa! Semoga harimu menyenangkan!";
+});
